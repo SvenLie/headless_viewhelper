a606c978614c68177427b14d0988098f74dc0ad4
Sven Liebert <mail@sven-liebert.de>
2025-03-24 17:55:56 +0100

    [FEATURE] Initial fluid core viewhelper

---
+++ ext_localconf.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('TYPO3') || die();
+
+call_user_func(
+    static function () {
+       $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['headlessViewHelper'] = [
+            'SvenLie\HeadlessViewhelper\ViewHelpers'
+        ];
+    }
+);
