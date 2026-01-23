3583b8d8398b167f76f7faff3f11cc227ed643e7
Sven Liebert <sven.liebert@telekom.de>
2026-01-23 10:58:55 +0100

    Add static headline

---
+++ Classes/ViewHelpers/Form/StaticHeadlineViewHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SvenLie\HeadlessViewhelper\ViewHelpers\Form;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+
+final class StaticHeadlineViewHelper extends AbstractTagBasedViewHelper
+{
+    protected array $data = [];
+
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('identifier', 'string', 'Specifies the id of the form element.', false, '');
+    }
+
+    public function render()
+    {
+        $this->data = $this->arguments;
+        $children = trim($this->renderChildren());
+        $this->data['type'] = 'staticHeadline';
+        $this->data['data'] = $children;
+        $this->data['value'] = $children;
+
+        return json_encode($this->data);
+    }
+}
